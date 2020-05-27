a49a634bcf2f5dbeb400dd8cec38802f5afb2b1d
Abdullah <abdullah.mohammed-int@dubizzle.com>
2020-05-27 20:41:26 +0400

    added listings and realtors

--- btre/settings.py
+++ btre/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'realtors.apps.RealtorsConfig',
 ]
 
 MIDDLEWARE = [
--- btre/urls.py
+++ btre/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
+    path('realtors/', include('realtors.urls')),
     path('admin/', admin.site.urls),
 ]
