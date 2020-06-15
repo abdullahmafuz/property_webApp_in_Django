be7bec7b993988ca1f01736fbde4e6aab1cd02e5
Abdullah <abdullah.mohammed-int@dubizzle.com>
2020-06-15 15:49:18 +0400

    updated api class

--- listings/urls.py
+++ listings/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='listings'),
     path('<int:listing_id>', views.listing, name='listing'),
         path('search', views.search, name='search'),
-        path('api', views.postListing, name='listing_api')
+        path('api', views.postListing, name='listing_api'),
+        path('api_class', views.ListingView.as_view(), name='listing_api_class'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -9,6 +9,10 @@ from .choices import bedroom_choices, price_choices, state_choices
 from django.core.files import File
 from django.core.files.temp import NamedTemporaryFile
 from urllib.request import urlopen
+from django.views import View
+from django.utils.decorators import method_decorator
+
+
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
     paginator = Paginator(listings, 6)
@@ -63,9 +67,42 @@ def search(request):
         'values': request.GET
     }
     return render(request, 'listings/search.html',context)
-@csrf_exempt
+
 def postListing(request):
     if request.method == 'POST':
+        payload = json.loads(request.body)
+        realtor_id = payload["realtor_id"]
+        title = payload["title"]
+        address = payload["address"]
+        city = payload["city"]
+        state = payload["state"]
+        zipcode = payload["zipcode"]
+        description = payload["description"]
+        price = payload["price"]
+        bedrooms = payload["bedrooms"]
+        bathrooms = payload["bathrooms"]
+        sqft = payload["sqft"]
+        lot_size = payload["lot_size"]
+        photo_main = payload["photo_main"]
+        realtor_obj = Realtor.objects.get(id=realtor_id)
+        img_temp = NamedTemporaryFile(delete = True)
+        img_temp.write(urlopen(photo_main).read())
+        img_temp.flush()
+        listing = Listing(realtor=realtor_obj, title=title, address=address, city=city, state=state, zipcode=zipcode , description=description, price=price, bedrooms=bedrooms, bathrooms=bathrooms, sqft=sqft, lot_size=lot_size)    
+        listing.photo_main.save(f'image_{title}', File(img_temp))
+        listing.save()
+        return JsonResponse([{'success':'proprty is added successfully', "title": title}], safe=False)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ListingView(View):
+
+
+    def get(self, request):
+        return JsonResponse([{'GET':'Try to use POST request to add property listing',}], safe=False)
+    
+
+    def post(self, request):
         payload = json.loads(request.body)
         realtor_id = payload["realtor_id"]
         title = payload["title"]
